4a1f8e01b2d304a6ab831bf287134c8033d44e91
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-08-06 20:37:26 +1000

    Completed basic boilerplate for App. will work to uniquifying it

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </link>
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/5.0.7/css/all.css">
+</head>
+
+<body class="bg-gray-200 h-screen leading-none font-sans">
+    @yield('content')
+</body>
+
+</html>
